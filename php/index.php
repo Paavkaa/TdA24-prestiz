6ebc3e8a9c5a31365725534a226377c4af6f09cc
Lukáš Dihel <lukindihel@gmail.com>
2023-11-14 20:02:26 +0100

    create endpoints for api and client

--- php/index.php
+++ php/index.php
@@ -1,4 +1,12 @@
 <?php
-echo "Hello TdA";
+require_once __DIR__.'/router.php';
 
-?>
+// Static GET
+// In the URL -> http://localhost:8080/api
+// The output -> Index
+get('/api', 'api/api.php');
+
+// Static GET
+// In the URL -> http://localhost:8080/
+// The output -> Index
+get('/', 'public/main.php');

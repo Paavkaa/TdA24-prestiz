12c5fc4e043029d1b3eb58a6c1088af0b15961b2
Lukáš Dihel <lukindihel@gmail.com>
2023-11-19 22:21:55 +0100

    First db model

--- database.sql
+++ database.sql
@@ -1,11 +1,46 @@
-create table db.test
+CREATE TABLE db.users
 (
-    column_name uuid null,
-    column_name2 int null
+    uuid           UUID        NOT NULL PRIMARY KEY,
+    title_before   VARCHAR(50),
+    first_name     VARCHAR(50) NOT NULL,
+    middle_name    VARCHAR(50),
+    last_name      VARCHAR(50) NOT NULL,
+    title_after    VARCHAR(50),
+    picture_url    VARCHAR(255),
+    location       VARCHAR(100),
+    claim          TEXT,
+    bio            TEXT,
+    price_per_hour INT
 );
 
-create table db.test22
+CREATE TABLE db.tags
 (
-    column_name uuid null,
-    column_name2 int null
+    uuid UUID        NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE db.users_tags
+(
+    user_uuid UUID NOT NULL,
+    tag_uuid  UUID NOT NULL,
+    PRIMARY KEY (user_uuid, tag_uuid),
+    FOREIGN KEY (user_uuid) REFERENCES db.users (uuid),
+    FOREIGN KEY (tag_uuid) REFERENCES db.tags (uuid)
+);
+
+
+CREATE TABLE db.telephone_numbers
+(
+    uuid      UUID        NOT NULL PRIMARY KEY,
+    user_uuid UUID        NOT NULL,
+    number    VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_uuid) REFERENCES db.users (uuid)
+);
+
+CREATE TABLE db.email_addresses
+(
+    uuid      UUID        NOT NULL PRIMARY KEY,
+    user_uuid UUID        NOT NULL,
+    email     VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_uuid) REFERENCES db.users (uuid)
 );

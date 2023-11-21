0f923e11dd63e0b4e509fdad24d541c0fccf980a
Paavkaa <terezapavelkova1@gmail.com>
2023-11-21 19:36:27 +0100

    creating lector page

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ $router = new Core\Router();
 
 $router->addRoute('GET', '/api', 'Api@get');
 $router->addRoute('GET','/api/lektor/{uuid}', 'Lektor@get');
+$router->addRoute('GET','lektor', 'Lektor@index');
 // Add more routes as needed
 
 $router->handleRequest();

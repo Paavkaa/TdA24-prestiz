8decdd59ced3e635fa77f6d9ade47b7c545d83a9
Lukáš Dihel <lukindihel@gmail.com>
2023-11-26 00:43:06 +0100

    Typo

--- public/index.php
+++ public/index.php
@@ -5,9 +5,10 @@ require "../vendor/autoload.php";
 $router = new Core\Router();
 
 $router->addRoute('GET', '/api', 'Api@get');
-$router->addRoute('GET', '/api/lektor/{uuid}', 'Lektor@get');
-$router->addRoute('GET','lektor', 'Lektor@index');
-$router->addRoute('GET', '/lektor', 'Lektor@index');
+$router->addRoute('GET', '/api/lecturer/{uuid}', 'Lektor@get');
+$router->addRoute('GET', '/api/lecturer', 'Lektor@post');
+$router->addRoute('GET', 'lecturer', 'Lektor@index');
+$router->addRoute('GET', '/lecturer', 'Lektor@index');
 // Add more routes as needed
 
 $router->handleRequest();
